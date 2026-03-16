a63fb1cf58b59e6c4b21d40bf212d810c168edbb
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-03-16 16:59:04 -0400

    Use chunk relative coords for tiles

--- src/scripts/worldGen.py
+++ src/scripts/worldGen.py
@@ -13,23 +13,23 @@ def generateChunk(x, y):
     #set up noise generator
     opensimplex.seed(seed)
     #iterate through each tile in the chunk
-    for y_pos in range(chunk_size):
-        for x_pos in range(chunk_size):
+    for yPos in range(chunk_size):
+        for xPos in range(chunk_size):
             #find x and y locations
-            target_x = x * chunk_size + x_pos
-            target_y = y * chunk_size + y_pos
+            targetX = x * chunk_size + xPos
+            targetY = y * chunk_size + yPos
             #defult tile is nothing
-            tile_type = 0
-            height = opensimplex.noise2(target_x / 30, target_y / 30)
+            tileType = 0
+            height = opensimplex.noise2(targetX / 30, targetY / 30)
             #use height to determine tile type
             #2 = water
             #1 = grass
             if height <= -0.3:
-                tile_type = 2
+                tileType = 2
                 #set interactabl;e item to zero because tile is water
             elif height > -0.3:
-                tile_type = 1
-            if tile_type != 0:
+                tileType = 1
+            if tileType != 0:
                 #if the tile is not nothing, add it to the chunk data
-                chunk_data.append([(target_x, target_y), tile_type])
+                chunk_data.append([(xPos, yPos), tileType])
     return chunk_data
